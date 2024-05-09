2be56b6e33c0a760d8210d1d066174861d230f82
yuu <yogistanada252@gmail.com>
2024-05-09 11:00:39 +0700

    chore: init transaction service

---
+++ transaction/index.ts
@@ -0,0 +1,54 @@
+import dotenv from "dotenv";
+import fastify from "fastify";
+import cors from "@fastify/cors";
+import formDataParser from "@fastify/formbody";
+
+import SupertokensEmailPassword from "supertokens-node/recipe/emailpassword";
+import Session from "supertokens-node/recipe/session";
+import { plugin as supertokenPlugin } from "supertokens-node/framework/fastify";
+import supertokens from "supertokens-node";
+import { verifySession } from "supertokens-node/recipe/session/framework/fastify";
+
+dotenv.config();
+
+const server = fastify();
+
+// init supertoken
+supertokens.init({
+  supertokens: {
+    connectionURI: process.env.SUPERTOKENS_URI ?? "",
+    apiKey: process.env.SUPERTOKENS_API_KEY,
+  },
+  appInfo: {
+    appName: process.env.APP_NAME ?? "",
+    websiteDomain: process.env.SUPERTOKENS_WEBSITE_DOMAIN ?? "",
+    apiDomain: process.env.SUPERTOKENS_API_DOMAIN ?? "",
+  },
+  recipeList: [Session.init()],
+});
+
+server.register(formDataParser);
+server.register(supertokenPlugin);
+server.register(cors, {
+  allowedHeaders: ["Content-Type", ...supertokens.getAllCORSHeaders()],
+  credentials: true,
+});
+
+server.get("/ping", async (req, reply) => {
+  return reply
+    .code(200)
+    .header("Content-Type", "application/json")
+    .send({ message: "pong" });
+});
+
+server.listen(
+  { port: process.env.APP_PORT ? Number(process.env.APP_PORT) : 3000 },
+  (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+
+    console.log(`server listening add ${address}`);
+  },
+);
